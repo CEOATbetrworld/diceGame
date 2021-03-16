3ae61ac0ffb7824007d76c9549fd3d3cd66c0c9c
DHRUV KUMAR <dkiphones620@gmail.com>
2021-03-16 16:37:07 +0530

    made dice game

---
+++ index.js
@@ -0,0 +1,29 @@
+// code for dice 1
+
+var x = Math.floor((Math.random() * 6) + 1);
+console.log(x);
+
+var srcString = "images/dice" + x + ".png";
+
+var dice1 = document.getElementsByClassName("img1")[0];
+
+dice1.setAttribute("src", srcString);
+
+// code for dice 2
+
+var y = Math.floor((Math.random() * 6) + 1);
+
+var srcString2 = "images/dice" + y + ".png";
+
+var dice2 = document.getElementsByClassName("img2")[0];
+
+dice2.setAttribute("src", srcString2);
+
+var winner = document.getElementsByTagName("h1")[0];
+
+if(x > y){
+  winner.innerHTML = "Player 1 Won";
+}
+else{
+  winner.innerHTML = "Player 2 Won";
+}
